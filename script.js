aff05acd440d364be041536c41903adc3c4885d3
NikolaiUr4 <nikolaiur4@gmail.com>
2020-04-25 16:32:46 +0700

    massage

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-$("#prev").click(function(e){
-    $("#slideShow").carousel('prev');
-});
+let i=false;
 
-$("#next").click(function(e){
+$("#slideShow").carousel({
+    interval:3000,
+    keyboard: false,
+    pause: i,
+    ride: true,
+    wrap: true
+})
+$("#slideShow").carousel('prev');
+
+$("#prev").click(function (e){
+    $("#slideShow").carousel('prev');
+})
+
+$("#slideShow").carousel('next');
+
+$("#next").click(function (e){
     $("#slideShow").carousel('next');
-});
+})
+
+$("#play").click(function(){
+    if(i) {
+        $("#slideShow").carousel('cycle');
+        i=false;
+        console.log(i);
+        $("#play").text('▶');
+    }
+    else {
+        $("#slideShow").carousel('pause');
+        i=true;
+        console.log(i);
+        $("#play").text('▌▌');
+    }
+})
+ 
